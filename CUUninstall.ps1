389a53794c0b8ecd9ba338e285ac508e374d033c
dwsservice <160760572+dwsservice@users.noreply.github.com>
2024-03-13 11:06:47 -0400

    Update CUUninstall.ps1

--- CUUninstall.ps1
+++ CUUninstall.ps1
@@ -1,6 +1,6 @@
-﻿${_____/=\/=\_/\___} = gwmi win32_product | where{$_.name -eq $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QwBvAG4AdAByAG8AbABVAHAAIABFAGQAZwBlACAARABYACAALQAgAFMASQBQACAAQQBnAGUAbgB0AA==')))}
-${_____/=\/=\_/\___}.IdentifyingNumber
-saps $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QwA6AFwAVwBpAG4AZABvAHcAcwBcAFMAeQBzAHQAZQBtADMAMgBcAG0AcwBpAGUAeABlAGMALgBlAHgAZQA='))) -ArgumentList "/x $(${_____/=\/=\_/\___}.IdentifyingNumber) /quiet /noreboot"
 ${_____/=\/=\_/\___} = gwmi win32_product | where{$_.name -eq $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QwBvAG4AdAByAG8AbABVAHAAIABBAGcAZQBuAHQAIABNAGEAbgBhAGcAZQByAA==')))}
 ${_____/=\/=\_/\___}.IdentifyingNumber
 saps $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QwA6AFwAVwBpAG4AZABvAHcAcwBcAFMAeQBzAHQAZQBtADMAMgBcAG0AcwBpAGUAeABlAGMALgBlAHgAZQA='))) -ArgumentList "/x $(${_____/=\/=\_/\___}.IdentifyingNumber) /quiet /noreboot" 
+${_____/=\/=\_/\___} = gwmi win32_product | where{$_.name -eq $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QwBvAG4AdAByAG8AbABVAHAAIABFAGQAZwBlACAARABYACAALQAgAFMASQBQACAAQQBnAGUAbgB0AA==')))}
+${_____/=\/=\_/\___}.IdentifyingNumber
+saps $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QwA6AFwAVwBpAG4AZABvAHcAcwBcAFMAeQBzAHQAZQBtADMAMgBcAG0AcwBpAGUAeABlAGMALgBlAHgAZQA='))) -ArgumentList "/x $(${_____/=\/=\_/\___}.IdentifyingNumber) /quiet /noreboot"
